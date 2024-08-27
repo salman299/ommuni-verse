a09a48c9d4f177bf265a7b722cab656e97e7e87d
Aliaan <aliaankhowaja@gmail.com>
2024-08-27 22:55:48 +0500

    feat: User detail, update endpoint added

--- app/core/api/v1/urls.py
+++ app/core/api/v1/urls.py
@@ -1,19 +1,24 @@
 """
 Root API URLs.
 """
+
 from django.urls import path, include
 from app.core.api.v1.views.login import RegisterView
 from app.core.api.v1.views.area import AreaListView, UniqueCitiesView
-from app.core.api.v1.views.user import PersonListView, PersonRetrieveView
+from app.core.api.v1.views.user import (
+    PersonListView,
+    PersonRetrieveView,
+    UserRetrieveUpdateView,
+)
 
-app_name = 'v1_apis'
+app_name = "v1_apis"
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('areas/', AreaListView.as_view(), name='area'),
-    path('areas-cities/', UniqueCitiesView.as_view(), name='area-cities'),
-    path('users/', PersonListView.as_view(), name='users'),
-    path('current-user/', PersonRetrieveView.as_view(), name='current-user'),
-    path('', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    # other URL patterns
+    path("register/", RegisterView.as_view(), name="register"),
+    path("areas/", AreaListView.as_view(), name="area"),
+    path("areas-cities/", UniqueCitiesView.as_view(), name="area-cities"),
+    path("users/", PersonListView.as_view(), name="users"),
+    path("user/", UserRetrieveUpdateView.as_view(), name="user"),
+    path("current-user/", PersonRetrieveView.as_view(), name="current-user"),
+    path("", include("oauth2_provider.urls", namespace="oauth2_provider")),
 ]
--- app/core/api/v1/views/user.py
+++ app/core/api/v1/views/user.py
@@ -1,15 +1,22 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+)
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from app.core.api.serializers.user import UserDetailUpdateSerializer
 from app.core.models import Person
 from app.core.api.serializers.user import PersonSerializer
+
 
 class PersonListView(ListAPIView):
     permission_classes = [IsAdminUser]
     serializer_class = PersonSerializer
     filter_backends = [SearchFilter]
-    search_fields = ['full_name', 'user__username']
+    search_fields = ["full_name", "user__username"]
     queryset = Person.objects.all()
+
 
 class PersonRetrieveView(RetrieveAPIView):
     permission_classes = [IsAuthenticated]
@@ -17,3 +24,12 @@ class PersonRetrieveView(RetrieveAPIView):
 
     def get_object(self):
         return self.request.user.person
+
+
+class UserRetrieveUpdateView(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserDetailUpdateSerializer
+
+    def get_object(self):
+        person = Person.objects.get(id=self.request.user.id)
+        return person
